cfa37939e8bb3fb6ef9aa4c0731769c0c021123b
MarcusRei <marcusreineck@gmail.com>
2023-01-25 17:39:22 +0100

    can create static usercart

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -14,6 +14,24 @@ const { assertValidExecutionArguments } = require("graphql/execution/execute");
 const usercartDirectory = path.join(__dirname, "..", "data", "usercarts");
 const itemDirectory = path.join(__dirname, "..", "data", "items");
 
+const basketball = {
+  id: "7b30fcf5-06cb-4f3c-b61e-b560e3476c5b",
+  name: "Basketball",
+  itemprice: 150,
+};
+
+const football = {
+  id: "7b30fcf5-06cb-4f3c-b61e-b560e3476c5b",
+  name: "Football",
+  itemprice: 100,
+};
+
+const tennisball = {
+  id: "7b30fcf5-06cb-4f3c-b61e-b560e3476c5b",
+  name: "Tennisball",
+  itemprice: 50,
+};
+
 exports.resolvers = {
   Query: {
     getUsercartById: async (_, args) => {
@@ -56,13 +74,93 @@ exports.resolvers = {
 
       return usercartData;
     },
-    getAllItems: async (_, args) => {
-      let items = [];
-    },
   },
   Mutation: {
-    createUsercart: async (_, args) => {},
-    updateUsercart: async (_, args) => {},
-    deleteUsercart: async (_, args) => {},
+    createUsercart: async (_, args) => {
+      const items = [basketball, tennisball, football];
+
+      let price = 0;
+      for (let i = 0; i < items.length; i++) {
+        price += items[i].itemprice;
+      }
+
+      //Skapa unikt id
+      const newUsercart = {
+        id: crypto.randomUUID(),
+        amountOfItems: items.length,
+        items: items,
+        totalPrice: price,
+      };
+
+      //Skapa filePath
+      let filePath = path.join(usercartDirectory, `${newUsercart.id}.json`);
+
+      let idExists = true;
+      while (idExists === true) {
+        const exists = await fileExists(filePath);
+        console.log(exists, newUsercart.id);
+
+        if (exists === true) {
+          newUsercart.id = crypto.randomUUID();
+          filePath = path.join(usercartDirectory, `${newUsercart.id}.json`);
+        }
+
+        idExists = exists;
+      }
+
+      await fsPromises.writeFile(filePath, JSON.stringify(newUsercart));
+
+      return newUsercart;
+
+      //return null;
+    },
+    addItemToUsercart: async (_, args) => {},
+    updateUsercart: async (_, args) => {
+      const { id, username, amountOfItems, totalPrice } = args;
+
+      const filePath = path.join(usercartDirectory, `${id}.json`);
+
+      const usercartExists = await fileExists(filePath);
+      if (!usercartExists)
+        return new GraphQLError("Den här varukorgen finns inte");
+
+      const updatedUsercart = {
+        id,
+        username,
+        amountOfItems,
+        totalPrice,
+      };
+
+      await fsPromises.writeFile(filePath, JSON.stringify(updatedUsercart));
+
+      return updatedUsercart;
+    },
+    deleteUsercart: async (_, args) => {
+      //Get the ID
+      const usercartId = args.usercartId;
+
+      const filePath = path.join(usercartDirectory, `${usercartId}.json`);
+
+      //Kolla att ID:et finns
+      const usercartExists = await fileExists(filePath);
+      if (!usercartExists) {
+        return new GraphQLError("Den här varukorgen finns inte");
+      }
+
+      //Delete file
+      try {
+        await deleteFile(filePath);
+      } catch (error) {
+        return {
+          deletedId: usercartId,
+          success: false,
+        };
+      }
+
+      return {
+        deletedId: usercartId,
+        success: true,
+      };
+    },
   },
 };
